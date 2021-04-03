71639c2c5455926f0ddda340c0b14016b701e7ed
RedFlowerYeah <346125735@qq.com>
2021-04-03 17:51:40 +0800

    fiftieth-fourth commit(     前端 增加天气预报界面自定义css样式，优化相应的天气预报界面 )

--- src/main/java/com/travelfourm/controller/WeatherController.java
+++ src/main/java/com/travelfourm/controller/WeatherController.java
@@ -9,6 +9,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.Date;
+
+/**
+ * @author 34612
+ */
 @RestController
 @RequestMapping("/report")
 public class WeatherController {
@@ -21,6 +26,7 @@ public class WeatherController {
     @GetMapping("/cityId/{cityId}")
     public ModelAndView getReportByCityId(@PathVariable("cityId") String cityId, Model model) throws Exception {
         model.addAttribute("title", "天气预报");
+        model.addAttribute("date",new Date());
         model.addAttribute("cityId", cityId);
         model.addAttribute("cityList", cityDataService.listCity());
         model.addAttribute("report", weatherReportService.getDataByCityId(cityId));
